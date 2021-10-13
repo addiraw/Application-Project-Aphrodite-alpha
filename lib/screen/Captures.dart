ba4fe8e81b7a1102e558c5f84da6961324b61b5d
aditya kumar <adityakumar1995rawat@gmail.com>
2021-10-13 16:13:11 +0530

    page added to the application

---
+++ lib/screen/Captures.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+class Captures extends StatefulWidget {
+  const Captures({ Key? key }) : super(key: key);
+
+  @override
+  State<Captures> createState() => _CapturesState();
+}
+
+class _CapturesState extends State<Captures> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("reached"),
+    );
+  }
+}
---
+++ lib/screen/SoilQuality.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import 'Captures.dart';
+
+class SoilQuality extends StatelessWidget {
+  const SoilQuality({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: MaterialApp(
+            home: Scaffold(
+                appBar: AppBar(
+                  title: Text("Soil Quality Check"),
+                ),
+                body: Container(
+                    alignment: Alignment.center,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Welcome to Virtual lab",
+                          style: TextStyle(
+                              color: Colors.blueAccent,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w700),
+                        ),
+                        Text("Proceed to check soil"),
+                        TextButton(
+                            onPressed: () {
+                              print("pressed");
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => Captures()));
+                            },
+                            child: Icon(Icons.arrow_forward)),
+                      ],
+                    )))));
+  }
+}
